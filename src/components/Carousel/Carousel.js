8f353b18055649ed7eea9e8d3168f014c26701a1
tobiajras <tobiajras@hotmail.com>
2021-12-13 21:08:01 -0300

    carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,10 +4,11 @@ import Carousel from 'react-bootstrap/Carousel';
 import image1 from '../../assets/fondoArena.jpg';
 import image2 from '../../assets/fondoPiedra.jpg';
 import image3 from '../../assets/fondoLadrillos.jpg';
+import image4 from '../../assets/fondoMallas.jpg';
 
 const CarouselContainer = () => {
   return(
-    <Carousel pause={false}>
+    <Carousel pause={false} dark={true}>
   <Carousel.Item interval={2000}>
     <img
       className="d-block w-100"
@@ -33,6 +34,16 @@ const CarouselContainer = () => {
       className="d-block w-100"
       src={image3}
       alt="Third slide"
+    />
+    <Carousel.Caption>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item interval={2000}>
+    <img
+      className="d-block w-100"
+      src={image4}
+      alt="fourth slide"
     />
     <Carousel.Caption>
     </Carousel.Caption>
